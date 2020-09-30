6ded83224a6a6edb36bfd0a2f9f998bdf71259f9
Dylan Chung <dylan.chung1997@gmail.com>
2020-09-29 23:44:41 -0700

    Adding New entry/Deletion with error checking

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
+
+//In order to access data needed for adding new entry, we need this json-parser.
+app.use(express.json())
 
 let persons = [
     {
@@ -28,7 +32,7 @@ let persons = [
 //Info 
 app.get('/api/info', (req, res) => {
     res.send('<span>Phonebook has info for ' +  persons.length + ' people </span> <br/>' +
-             '<span>' + new Date().getTime().toString() +'</span>')
+             '<span>' + new Date().toString() +'</span>')
     
   })
   
@@ -43,10 +47,57 @@ app.get('/api/persons/:id', (request, response) => {
     console.log('id is : ' + id)
     const person = persons.find(element => { return element.id === id})
     console.log(person)
-    response.json(person)
+
+    //If an entry for the given id is not found, the server has to respond with the appropriate status code.
+    if(person){
+        response.json(person)
+    } else{
+        response.status(404).end()
+    }
   })
 
+  //Deleting a single phonebook entry
+  app.delete('/api/persons/:id', (req, res) => {
+      const id = Number(req.params.id)
+      persons = persons.filter(element => element.id !== id)
 
+      //Even if the resource didn't originally exist, there's no consensus on what code should be returned
+      //so in both cases, let's just return 204.
+      response.status(204).end()
+  })
+
+  const generateID = () => {
+    const Id = persons.length > 0 ? Math.max(...persons.map(ele => ele.id))+1: 0
+    return Id
+  }
+
+  //Adding new entry 
+  app.post('/api/persons', (req, res) => {
+      const body = req.body
+      const nameFilter = persons.map(ele => ele.name)
+
+      //Error Handling if NAME or NUMBER is missing, OR NAME already exists
+      if (!body.name || !body.number) {
+        return res.status(400).json({ 
+          error: 'Name or Number is missing' 
+        })
+      } if(nameFilter.includes(body.name)){
+        return res.status(400).json({ 
+            error: 'name must be unique' 
+          })
+      }
+
+      const person = {
+        name : body.name,
+        number: body.number,
+        id : generateID(),
+      }
+      
+      console.log("Name:", person.name)
+      persons = persons.concat(person)
+      
+      res.json(person)
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
