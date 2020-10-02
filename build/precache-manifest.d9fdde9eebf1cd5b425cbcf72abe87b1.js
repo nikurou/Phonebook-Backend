c3f4044e8175bf23ce6002161211eb13f0d3b826
Dylan Chung <dylan.chung1997@gmail.com>
2020-10-01 20:21:34 -0700

    npm run build from front-end copied over to back

---
+++ build/precache-manifest.d9fdde9eebf1cd5b425cbcf72abe87b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d81b196ef7ab68382a2c365f4f0a278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a9082d88876e4ac5cd",
+    "url": "/static/css/main.7d5cd7d8.chunk.css"
+  },
+  {
+    "revision": "9af68b89fbfe292cb182",
+    "url": "/static/js/2.c7d8749e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7d8749e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a9082d88876e4ac5cd",
+    "url": "/static/js/main.07da1974.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
