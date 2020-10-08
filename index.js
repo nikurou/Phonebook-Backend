55cb40720d06d1a4a46fe9e0357ebd22451bf507
Dylan Chung <dylan.chung1997@gmail.com>
2020-10-07 19:54:54 -0700

    Error handling, Deletion, and Updating entry added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config() //This must be imported before the Person model, otherwise Person won't have an env variable or address to work off of.
 const express = require('express')
 const app = express()
-const cors = require('cors') 
+const cors = require('cors')
 const Person = require('./models/person')
 
 var morgan = require('morgan')
 const { response } = require('express')
 
-morgan.token('body', function (req){
+morgan.token('body', function (req) {
   return JSON.stringify(req.body)
 })
 
@@ -16,65 +16,101 @@ app.use(express.json()) //JSON PARSER
 app.use(express.static('build')) //To make Express show static content, the index HTML and JS, etc. 
 app.use(cors()) //Cross-Origin Resource Sharing (CORS) allows restricted resources on a webpage to be requested from another domain. 
 
-let persons = []
 
 //Info 
 app.get('/api/info', (req, res) => {
-    res.send('<span>Phonebook has info for ' +  persons.length + ' people </span> <br/>' +
-             '<span>' + new Date().toString() +'</span>')
-    
-  })
-  
+  res.send('<span>Phonebook has info for ' + Person.length + ' people </span> <br/>' +
+    '<span>' + new Date().toString() + '</span>')
+
+})
+
 //Fetching all resources
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(person => {
-      res.json(person)
-    })
+  Person.find({}).then(person => {
+    res.json(person)
+  })
 })
 
 //Fetching a single person resource
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
-    }).catch(error =>response.status(404).end())
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        // Person not found
+        response.status(404).end()
+      }
+    }).catch(error => next(error))
 })
 
-  //Deleting a single phonebook entry
-  app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(element => element.id !== id)
+//Deleting a single phonebook entry
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end() //returns 204 no content after deletion
+    })
+    .catch(error => next(error))
+})
 
-    //Even if the resource didn't originally exist, there's no consensus on what code should be returned
-    //so in both cases, let's just return 204.
-    res.status(204).end()
+//Adding new entry to MongoDB
+app.post('/api/persons', (req, res) => {
+  const body = req.body
 
-    console.log(`AFTER DELETING: ${persons}`)
+  if (body.name === undefined && body.number === undefined) {
+    return res.status(400).json({
+      error: 'Name or number is missing'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
   })
 
-  //Adding new entry to MongoDB
-  app.post('/api/persons', (req, res) => {
-      const body = req.body
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+})
+
+// Modifying an already existing entry
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
   
-      if (body.name === undefined && body.number === undefined) {
-        return res.status(400).json({ 
-          error: 'Name or number is missing' 
-        })
-      } 
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-      const person = new Person({
-        name : body.name,
-        number: body.number,
-      })
-      
-      person.save().then(savedPerson => {
-        res.json(savedPerson)
-      })
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  .then(updatedPerson => {
+    response.json(updatedPerson)
   })
-  
-  const PORT = process.env.PORT 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+  .catch(error => next(error))
+})
 
-  //Uses NodeExpress and Nodemon
+//Middleware for error and unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
 
