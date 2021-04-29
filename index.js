2642d7a6c30e9a5545a85db509d70547826d68f1
nikurou <dylan.chung1997@gmail.com>
2021-04-29 15:07:19 -0700

    undo update

--- index.js
+++ index.js
@@ -1,121 +1,121 @@
-require('dotenv').config() //This must be imported before the Person model, otherwise Person won't have an env variable or address to work off of.
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Person = require('./models/person')
+require("dotenv").config(); //This must be imported before the Person model, otherwise Person won't have an env variable or address to work off of.
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Person = require("./models/person");
 
-var morgan = require('morgan')
-const { response } = require('express')
+var morgan = require("morgan");
+const { response } = require("express");
 
-morgan.token('body', function (req) {
-  return JSON.stringify(req.body)
-})
+morgan.token("body", function (req) {
+  return JSON.stringify(req.body);
+});
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')) //Console Logging format with Morgan
-app.use(express.json()) //JSON PARSER
-app.use(express.static('build')) //To make Express show static content, the index HTML and JS, etc. 
-app.use(cors()) //Cross-Origin Resource Sharing (CORS) allows restricted resources on a webpage to be requested from another domain. 
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+); //Console Logging format with Morgan
+app.use(express.json()); //JSON PARSER
+app.use(express.static("build")); //To make Express show static content, the index HTML and JS, etc.
+app.use(cors()); //Cross-Origin Resource Sharing (CORS) allows restricted resources on a webpage to be requested from another domain.
 
-
-//Info 
-app.get('/api/info', (req, res) => {
-  res.send('<span>Phonebook has info for ' + Person.length + ' people </span> <br/>' +
-    '<span>' + new Date().toString() + '</span>')
-
-})
+//Info
+app.get("/api/info", (req, res) => {
+  res.send(
+    "<span>Phonebook has info for " +
+      Person.length +
+      " people </span> <br/>" +
+      "<span>" +
+      new Date().toString() +
+      "</span>"
+  );
+});
 
 //Fetching all resources
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(person => {
-    res.json(person)
-  })
-})
+app.get("/api/persons", (req, res) => {
+  Person.find({}).then((person) => {
+    res.json(person);
+  });
+});
 
 //Fetching a single person resource
-app.get('/api/persons/:id', (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   Person.findById(request.params.id)
-    .then(person => {
+    .then((person) => {
       if (person) {
-        response.json(person)
+        response.json(person);
       } else {
         // Person not found
-        response.status(404).end()
+        response.status(404).end();
       }
-    }).catch(error => next(error))
-})
+    })
+    .catch((error) => next(error));
+});
 
 //Deleting a single phonebook entry
-app.delete('/api/persons/:id', (request, response, next) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end() //returns 204 no content after deletion
+    .then((result) => {
+      response.status(204).end(); //returns 204 no content after deletion
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 //Adding new entry to MongoDB
-app.post('/api/persons', (req, res, next) => {
-  const body = req.body
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
 
   if (body.name === undefined && body.number === undefined) {
     return res.status(400).json({
-      error: 'Name or number is missing'
-    })
+      error: "Name or number is missing",
+    });
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  })
+  });
 
-  person.save()
-  .then(savedPerson => savedPerson.toJSON())
-  .then(savedAndFormattedPerson => {
-    res.json(savedAndFormattedPerson)
-  })
-  .catch(error=>next(error))
-})
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
+});
 
 // Modifying an already existing entry
-app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-  
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
   const person = {
     name: body.name,
     number: body.number,
-  }
+  };
 
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
-  .then(updatedPerson => {
-    response.json(updatedPerson)
-  })
-  .catch(error => next(error))
-})
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 
 //Middleware for error and unknown endpoint
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 // handler of requests with unknown endpoint
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return response.status(400).send({ error: "malformatted id" });
   }
-  next(error)
-}
+  next(error);
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
-
+  console.log(`Server running on port ${PORT}`);
+});
--- models/person.js
+++ models/person.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log("connecting to", url);
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('connected to MongoDB')
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 // Each schema maps to a MongoDB collection and defines the shape of the documents within that collection.
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+  name: String,
+  number: String,
+  id: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString(); //returnobject.id is actually an object, so toJSON and toString it, then save
+    delete returnedObject._id; // MongoDB's generated v and id are not needed.
+    delete returnedObject.__v;
   },
-  number: {
-    type: String,
-    required: true
-  }, 
-  id: String
-})
+});
 
-personSchema.set('toJSON',{
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString() //returnobject.id is actually an object, so toJSON and toString it, then save
-        delete returnedObject._id // MongoDB's generated v and id are not needed.
-        delete returnedObject.__v
-    }
-})
-
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema);
